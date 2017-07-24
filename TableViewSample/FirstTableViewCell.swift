f59739f758ea3a2ea5d281c72b073da69d3018cd
sdsb8432 <sdsb8432@gmail.com>
2017-07-24 14:20:47 +0900

    이미지 크기별로 Cell 크기 조정

---
+++ TableViewSample/FirstTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FirstTableViewCell.swift
+//  TableViewSample
+//
+//  Created by 손성빈 on 2017. 7. 24..
+//  Copyright © 2017년 seongbinson. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableViewSample/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TableViewSample
+//
+//  Created by 손성빈 on 2017. 7. 24..
+//  Copyright © 2017년 seongbinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let imageName = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    let imageWidth = [1920, 1920, 1920, 1920, 1920, 1920, 1920, 1280]
+    let imageHeight = [960, 1220, 1280, 1334, 1280, 1280, 1032, 720]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        tableView.rowHeight = UITableViewAutomaticDimension
+//        tableView.estimatedRowHeight = 300
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageName.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "first", for: indexPath) as! FirstTableViewCell
+        
+        let ratio = CGFloat(imageWidth[indexPath.row]) / tableView.frame.width
+        
+        cell.photoView.frame = CGRect(x: 0, y: 0, width: tableView.frame.width, height: CGFloat(imageHeight[indexPath.row]) / ratio)
+        cell.photoView.image = UIImage(named: imageName[indexPath.row])
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let ratio = CGFloat(imageWidth[indexPath.row]) / tableView.frame.width
+        
+        return CGFloat(imageHeight[indexPath.row]) / ratio
+    }
+    
+}
+
